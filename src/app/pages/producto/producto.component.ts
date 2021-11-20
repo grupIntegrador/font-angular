d45d03746d0c531fa88010714a2d52cc08cd590c
kevettt <kevinx.23.96@gmail.com>
2021-11-20 13:39:08 -0500

    v0.7

--- src/app/pages/producto/producto.component.ts
+++ src/app/pages/producto/producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
 import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
@@ -8,13 +11,31 @@ import { ProductoService } from 'src/app/services/producto.service';
 })
 export class ProductoComponent implements OnInit {
 
-  constructor(private productoService:ProductoService) { }
+  producto !:Producto;
+
+  constructor(
+    private productoService:ProductoService,
+    private activatedRute:ActivatedRoute,
+    private toaster:ToastrService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRute.snapshot.params.id;
+    this.productoService.detail(id).subscribe(
+      data=>{
+        this.producto =data;
+      },err=>{
+        this.toaster.error(err.error.mensaje,'Fail',{
+          timeOut:3000
+        });
+        this.volver()
+      }
+    )
   }
 
-  getProducto():void{
-    
+  volver():void{
+    this.router.navigate(['/lista'])
   }
 
 }
--- src/app/pages/update-producto/update-producto.component.ts
+++ src/app/pages/update-producto/update-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-update-producto',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProductoComponent implements OnInit {
 
-  constructor() { }
+  producto!: Producto;
+
+  constructor(
+    private productoService:ProductoService,
+    private activatedRute:ActivatedRoute,
+    private toaster:ToastrService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRute.snapshot.params.id;
+    this.productoService.detail(id).subscribe(
+      data=>{
+        this.producto=data
+      },err =>{
+        this.toaster.error(err.error.mensaje,'Fail',{
+          timeOut:3000
+        });
+        this.router.navigate(['/admin-list'])
+      }
+    )
+  }
+
+
+  onUpdate():void{
+    const id = this.activatedRute.snapshot.params.id;
+    this.productoService.update(id,this.producto).subscribe(
+      data=>{
+        this.toaster.success('Producto actualizado','OK',{
+          timeOut:3000
+        })
+        this.router.navigate(['admin-list']);
+      },err => {
+        this.toaster.error(err.error.mensaje,'Fail',{
+          timeOut:3000
+        })
+      }
+    )
   }
 
 }
